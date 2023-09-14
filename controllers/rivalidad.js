944ba074bb655f31029139b1141ede7da4dc5be7
Luis Chacon <luis.chacon1@outlook.com>
2023-09-14 10:20:34 -0700

    Reorder jugadores routes

--- controllers/rivalidad.js
+++ controllers/rivalidad.js
@@ -2,7 +2,7 @@ const { getDb } = require("../app");
 
 exports.getRivalidad = (req, res) => {
   const db = getDb();
-  const { id1, id2 } = req.params;
+  const { id1, id2 } = req.query;
 
   if (id1 && id2) {
     // Do something with id1 and id2
