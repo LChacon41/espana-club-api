8f399965cfd5c62acc74a9c78a91762cd1512dc6
Luis Chacon <luis.chacon1@outlook.com>
2023-09-20 14:21:35 -0700

    Fix bd promise connection

--- controllers/jugadores.js
+++ controllers/jugadores.js
@@ -1,10 +1,14 @@
 const { pool } = require("../config/db");
 
-
 exports.getAllJugadores = async (req, res) => {
   const query = "SELECT * FROM medida_jugadores";
-  let results = await pool.query(query); // Added 'let'
-  res.json(results);
+  try {
+    const [results, fields] = await pool.execute(query); // Using pool.execute() for promise-based API
+    res.json(results);
+  } catch (error) {
+    console.error('Error fetching jugadores:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 };
 
 exports.getJugadorById = async (req, res) => {
@@ -12,23 +16,31 @@ exports.getJugadorById = async (req, res) => {
   const { id } = req.params;
   const isEmail = id.includes("@");
 
-  let results = await pool.query(query); // Added 'let'
-
-  if (results.length === 0) {
-    res.status(404).json({ message: "Jugador no encontrado" });
-    return;
+  try {
+    const [results, fields] = await pool.execute(query, [id]); // Pass values as an array
+    if (results.length === 0) {
+      res.status(404).json({ message: "Jugador no encontrado" });
+      return;
+    }
+    const jugador = results[0];
+    res.json(jugador);
+  } catch (error) {
+    console.error('Error fetching jugadores:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
-  const jugador = results[0];
-  res.json(jugador);
 };
 
 exports.createJugador = async (req, res) => {
   const { nombre_completo, rama } = req.body;
-  const query =
-    "INSERT INTO medida_jugadores (nombre_completo, rama) VALUES (?, ?)";
+  const query = "INSERT INTO medida_jugadores (nombre_completo, rama) VALUES (?, ?)";
 
-  let result = await pool.query(query); // Added 'let'
-  res.status(201).json({ id: result.insertId, nombre_completo, rama });
+  try {
+    const [result, fields] = await pool.execute(query, [nombre_completo, rama]); // Pass values as an array
+    res.status(201).json({ id: result.insertId, nombre_completo, rama });
+  } catch (error) {
+    console.error('Error creating jugador:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 };
 
 
